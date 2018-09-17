f0e68d958e293891f0f83ba9ca5e5b453e9427b5
Felicity Hughes <Felicity.Hughes@intelematics.com>
2018-09-17 10:06:27 +1000

    Altered to stop Pip3 caching and dying.

--- PipUpdate.sh
+++ PipUpdate.sh
@@ -2,11 +2,11 @@
 
 ################################################################################
 # This script keeps Python packages installed via pip up-to-date.  Prefer to
-# update Python 3 packages first.
+# update Python 3 packages first - some caching issues in Pip3, Pip2 seems OK.
 #
 # Beware if you choose to install editable packages.  See this post for details:
 # https://stackoverflow.com/questions/2720014/upgrading-all-packages-with-pip
 ################################################################################
 
-pip3 list --outdated | cut -d ' ' -f1 | xargs -n1 pip3 install -U
-pip2 list --outdated | cut -d ' ' -f1 | xargs -n1 pip2 install -U
+/usr/local/bin/pip3 list --no-cache-dir --outdated | cut -d ' ' -f1 | xargs -n1 /usr/local/bin/pip3 install -U
+/usr/local/bin/pip2 list --outdated | cut -d ' ' -f1 | xargs -n1 /usr/local/bin/pip2 install --upgrade pip
