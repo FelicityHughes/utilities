ef721e463578d8b5156ced6c963681f75cdae392
Felicity Hughes <Felicity.Hughes@intelematics.com>
2018-12-17 08:44:49 +1100

    Cleaned up Ruby update script so it does not attempt version re-install.
    Preventing version re-install should allow this to play nicely with cron.  Now writes out to screen which version is installed and notes if it is not the latest - helpful for cron status report mail out.  Previous warning there in case installation attempt with anti-virus present causes issues.

--- RubyUpdate.sh
+++ RubyUpdate.sh
@@ -7,15 +7,26 @@
 ################################################################################
 
 
-LATEST_VERSION="$(rbenv install -l | grep -v - | tail -1 | tr -d '[:space:]')"
-rbenv install "${LATEST_VERSION}"
-RESULT=${?}
+LATEST_VERSION="$("rbenv" "install" "-l" | "grep" "-v" "-" | "tail" "-1" | "tr" "-d" '[:space:]')"
+VERSION_MATCH="$("rbenv" "versions" | "grep" "${LATEST_VERSION}")"
 
-if [[ ${RESULT} -eq 0 ]]
-then
-  rbenv local "${LATEST_VERSION}"
-  echo "Ruby version now ${LATEST_VERSION}.  Don't forget to uninstall obsolete versions:"
-  rbenv versions
+if [[ "${VERSION_MATCH}" == "" ]]; then
+  rbenv install "${LATEST_VERSION}"
+  RESULT=${?}
+
+  if [[ ${RESULT} -eq 0 ]]; then
+    rbenv global "${LATEST_VERSION}"
+    echo "Ruby version now ${LATEST_VERSION}.  Don't forget to uninstall obsolete versions:"
+    rbenv versions
+  else
+    echo "If you chose '(y)es', you may need to turn off anti-virus protection and re-try."
+  fi
 else
-  echo "If you chose '(y)es', you may need to turn off anti-virus protection and re-try."
+  VERSION_MATCH="$("rbenv" "versions" | "awk" '$1 == "*" {print $2}')"
+
+  if [[ "${VERSION_MATCH}" == "${LATEST_VERSION}" ]]; then
+    echo "Latest Ruby version ${LATEST_VERSION} is already installed and in use."
+  else
+    echo "Latest Ruby version ${LATEST_VERSION} is installed but version ${VERSION_MATCH} currently in use."
+  fi
 fi
